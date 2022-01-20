791af3e08760f704d471023f2e05efba20ca0109
ZPTX4869 <2532827403@qq.com>
2022-01-20 22:41:51 +0800

    2022-01-20

--- leetcode/searchs/findmin.go
+++ leetcode/searchs/findmin.go
@@ -15,3 +15,34 @@ func findMin(nums []int) int {
 
 	return nums[left]
 }
+
+func findMin2(nums []int) int {
+	left, right := 0, len(nums)-1
+
+	for left < right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == nums[right] {
+			if searchRight(nums, mid, right) {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		} else if nums[mid] < nums[right] {
+			right = mid
+		} else if nums[mid] > nums[right] {
+			left = mid + 1
+		}
+	}
+
+	return nums[left]
+}
+
+func searchRight(nums []int, start, end int) bool {
+	for cur := start; cur < end; cur++ {
+		if nums[cur] < nums[start] {
+			return true
+		}
+	}
+	return false
+}
--- leetcode/searchs/findmin_test.go
+++ leetcode/searchs/findmin_test.go
@@ -11,3 +11,10 @@ func Test_findMin(t *testing.T) {
 
 	assert.Equal(t, 1, res)
 }
+
+func Test_findMin2(t *testing.T) {
+	nums := []int{10, 1, 10, 10, 10}
+	res := findMin2(nums)
+
+	assert.Equal(t, 1, res)
+}
