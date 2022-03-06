d72b832054c20033ce3d563dbb6f77d200c539aa
ZPTX4869 <2532827403@qq.com>
2022-03-06 21:33:06 +0800

    2022-03-03++

--- leetcode/backtracks/permute.go
+++ leetcode/backtracks/permute.go
@@ -1,5 +1,10 @@
 package backtracks
 
+import (
+	"math"
+	"sort"
+)
+
 func permute(nums []int) [][]int {
 	res := make([][]int, 0)
 	path := make([]int, 0)
@@ -15,17 +20,17 @@ func permute(nums []int) [][]int {
 
 		for i := 0; i < len(nums); i++ {
 			// 如果已经将该元素添加到集合，则跳过
-			if used[nums[i]] {
+			if used[i] {
 				continue
 			}
 
 			path = append(path, nums[i])
-			used[nums[i]] = true
+			used[i] = true
 
 			backtrack()
 
 			path = path[0 : len(path)-1]
-			used[nums[i]] = false
+			used[i] = false
 		}
 	}
 
@@ -35,5 +40,33 @@ func permute(nums []int) [][]int {
 }
 
 func permuteUnique(nums []int) [][]int {
-	return nil
+	res := make([][]int, 0)
+	path := make([]int, 0)
+	used := make(map[int]bool, 0)
+
+	var backtrack func()
+	backtrack = func() {
+		if len(path) == len(nums) {
+			res = append(res, append([]int{}, path...))
+		}
+
+		for i := 0; i < len(nums); i++ {
+			if used[i] || i > 0 && nums[i]==nums[i-1] && !used[i-1] {
+				continue
+			}
+
+			path = append(path, nums[i])
+			used[i] = true
+
+			backtrack()
+
+			path = path[:len(path)-1]
+			used[i] = false
+		}
+	}
+
+    sort.Ints(nums)
+	backtrack()
+
+	return res
 }
