d437f3d20b43251033dea865d7e4bcdd4a177a1b
ZPTX4869 <2532827403@qq.com>
2022-03-12 18:00:53 +0800

    2022-03-12

--- offer/constant.go
+++ offer/constant.go
@@ -1,5 +1,12 @@
 package offer
 
-import "algorithm-go/structure/linkedlist"
+import (
+	"algorithm-go/structure/binarytree"
+	"algorithm-go/structure/linkedlist"
+	"math"
+)
+
+const null = math.MaxInt64
 
 type ListNode = linkedlist.ListNode
+type TreeNode = binarytree.TreeNode
