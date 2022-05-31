3d87de59eae6fe15542341e0d5b0ceea568726e6
sihaixianyu <2532827403@qq.com>
2022-05-31 23:29:11 +0800

    2022-05-31+

--- main.go
+++ main.go
@@ -2,26 +2,8 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 )
 
-var mu sync.Mutex
-var set = make(map[int]bool, 0)
-
-func printOnce(num int) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	if _, exist := set[num]; !exist {
-		fmt.Println(num)
-	}
-	set[num] = true
-}
-
 func main() {
-	for i := 0; i < 10; i++ {
-		go printOnce(100)
-	}
-	time.Sleep(time.Second)
+	fmt.Println(3 / 2)
 }
