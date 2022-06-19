23d6a81e8937a37e1c9beb2210be0c8aee01e2ca
sihaixianyu <2532827403@qq.com>
2022-06-19 19:28:29 +0800

    2022-06-19

--- leetcode/tree/build_tree.go
+++ leetcode/tree/build_tree.go
@@ -17,11 +17,55 @@ func buildTree(preorder []int, inorder []int) *TreeNode {
 
 	// 前序数组确定左子树边界元素索引：必须通过中序数组rootVal左侧的元素个数确定
 	rootVal := preorder[0]
-	idx := search(inorder, rootVal)
+	idxRoot := search(inorder, rootVal)
 
 	root := &TreeNode{Val: rootVal}
-	root.Left = buildTree(preorder[1:1+idx], inorder[:idx])
-	root.Right = buildTree(preorder[1+idx:], inorder[idx+1:])
+	root.Left = buildTree(preorder[1:idxRoot+1], inorder[:idxRoot])
+	root.Right = buildTree(preorder[idxRoot+1:], inorder[idxRoot+1:])
 
 	return root
+}
+
+func buildTree2(inorder []int, postorder []int) *TreeNode {
+	if len(inorder) == 0 {
+		return nil
+	}
+
+	rootVal := postorder[len(postorder)-1]
+	idxRoot := search(inorder, rootVal)
+
+	root := &TreeNode{Val: rootVal}
+	root.Left = buildTree2(inorder[:idxRoot], postorder[:idxRoot])
+	root.Right = buildTree2(inorder[idxRoot+1:], postorder[idxRoot:len(postorder)-1])
+
+	return root
+}
+
+func buildTree3(preorder []int, postorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+
+	if len(preorder) == 1 {
+		return &TreeNode{Val: preorder[0]}
+	}
+
+	rootVal := preorder[0]
+	idxLeft := search(postorder, preorder[1])
+
+	root := &TreeNode{Val: rootVal}
+	root.Left = buildTree3(preorder[1:2+idxLeft], postorder[:idxLeft+1])
+	root.Right = buildTree3(preorder[2+idxLeft:], postorder[idxLeft+1:len(postorder)-1])
+
+	return root
+}
+
+func search (nums []int, tar int) int {
+	for i, v := range nums {
+		if v == tar {
+			return i
+		}
+	}
+
+	return -1
 }
--- leetcode/tree/build_tree_test.go
+++ leetcode/tree/build_tree_test.go
@@ -50,3 +50,61 @@ func Test_buildTree(t *testing.T) {
 		})
 	}
 }
+
+func Test_buildTree2(t *testing.T) {
+	type args struct {
+		inorder   []int
+		postorder []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+		{
+			"case1",
+			args{
+				inorder:   []int{9, 3, 15, 20, 7},
+				postorder: []int{9, 15, 7, 20, 3},
+			},
+			binarytree.FromSlice([]int{3, 9, 20, null, null, 15, 7}).Root,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildTree2(tt.args.inorder, tt.args.postorder); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_buildTree3(t *testing.T) {
+	type args struct {
+		preorder  []int
+		postorder []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want *TreeNode
+	}{
+		// TODO: Add test cases.
+		{
+			"case1",
+			args{
+				preorder:  []int{1, 2, 4, 5, 3, 6, 7},
+				postorder: []int{4, 5, 2, 6, 7, 3, 1},
+			},
+			binarytree.FromSlice([]int{1, 2, 3, 4, 5, 6, 7}).Root,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := buildTree3(tt.args.preorder, tt.args.postorder); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("buildTree3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
